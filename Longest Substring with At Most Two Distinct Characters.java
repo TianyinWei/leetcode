1ae6e2280e87cfbd18bff8b4b4316cd685e7e2e0
TianyinWei <TianyinWei@users.noreply.github.com>
2016-05-22 10:28:00 -0700

    Update Longest Substring with At Most Two Distinct Characters.java

--- Longest Substring with At Most Two Distinct Characters.java
+++ Longest Substring with At Most Two Distinct Characters.java
@@ -1,7 +1,18 @@
+/*
+Given a string, find the length of the longest substring T that contains at most 2 distinct characters.
+
+For example, Given s = “eceba”,
+
+T is "ece" which its length is 3.
+*/
 public class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         /*
-        The main idea is to maintain a sliding window with 2 unique characters. The key is to store the last occurrence of each character as the value in the hashmap. This way, whenever the size of the hashmap exceeds 2, we can traverse through the map to find the character with the left most index, and remove 1 character from our map. Since the range of characters is constrained, we should be able to find the left most index in constant time.
+        The main idea is to maintain a sliding window with 2 unique characters. 
+        The key is to store the last occurrence of each character as the value in the hashmap.
+        This way, whenever the size of the hashmap exceeds 2, 
+        we can traverse through the map to find the character with the left most index, and remove 1 character from our map. 
+        Since the range of characters is constrained, we should be able to find the left most index in constant time.
         */
         
         if (s.length() <= 1) return s.length();
