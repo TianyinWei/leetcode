e8aa8df7a1e1476176143197799224c4a754eac0
TianyinWei <TianyinWei@users.noreply.github.com>
2016-07-01 10:46:39 -0700

    Create Meeting Rooms.java

---
+++ Meeting Rooms.java
@@ -0,0 +1,35 @@
+/*
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return false.
+*/
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0 || intervals.length == 1)    return true;
+        int len = intervals.length;
+        int[] starts = new int[len];
+        int[] ends = new int[len];
+        for (int i = 0; i < len; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        for (int i = 1; i < len; i++) {
+            if (starts[i] < ends[i-1])  return false;
+        }
+        return true;
+    }
+}
